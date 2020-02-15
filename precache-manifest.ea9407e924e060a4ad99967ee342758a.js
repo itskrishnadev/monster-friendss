4c2801b5f9ca4001b19b8bce506910d6b6122c67
Krishnadev1999 <iamkrishnadev1999@gmail.com>
2020-02-15 16:25:52 +0530

    Updates

---
+++ precache-manifest.ea9407e924e060a4ad99967ee342758a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b028993555fcb3678ca91f588b4fa4ef",
+    "url": "/monster-friendss/index.html"
+  },
+  {
+    "revision": "8eee1eaff33d84ac1192",
+    "url": "/monster-friendss/static/css/main.588efbee.chunk.css"
+  },
+  {
+    "revision": "cdd428ac6cdcaad393ee",
+    "url": "/monster-friendss/static/js/2.f747b30f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-friendss/static/js/2.f747b30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eee1eaff33d84ac1192",
+    "url": "/monster-friendss/static/js/main.e2df8362.chunk.js"
+  },
+  {
+    "revision": "d17b19038925d87a8eac",
+    "url": "/monster-friendss/static/js/runtime-main.64c374b2.js"
+  }
+]);
